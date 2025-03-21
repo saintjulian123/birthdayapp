62447c0b1e8591dad013632edc28bbfa28b3b931
saintjulian123 <202000136@ub.ac.bw>
2025-03-21 19:07:15 +0200

    fix

--- her/src/components/ui/dropdown-menu.tsx
+++ her/src/components/ui/dropdown-menu.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
-import { CheckIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import { CheckIcon } from '@radix-ui/react-icons'
 import { cn } from '@/lib/utils'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
